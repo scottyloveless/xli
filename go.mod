77373a06fe8bb9df559bb48894cdc31667c20616
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2025-07-19 15:13:26 -0500

    tidying up before reinstall

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/scottyloveless/xli
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type apiConfig struct {
+	key    string
+	secret string
+	bearer string
+}
+
+func main() {
+	// apiKey := os.Getenv("KEY")
+	// secret := os.Getenv("SECRET")
+	// bearer := os.Getenv("BEARER")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: xli \"your post here\"")
+		os.Exit(1)
+	}
+
+	input := os.Args[1]
+	if input == "login" {
+		// LoginHandler()
+	}
+
+	// RefreshToken if needed
+	// SendPost
+
+	fmt.Printf("You passed: %s\n", input)
+}
