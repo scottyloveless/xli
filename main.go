e0ddc0cfb41552c2c2dc9a863ddd3300ed201642
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2025-08-19 09:56:36 -0500

    latest

--- main.go
+++ main.go
@@ -15,16 +15,15 @@ func main() {
 	// apiKey := os.Getenv("KEY")
 	// secret := os.Getenv("SECRET")
 	// bearer := os.Getenv("BEARER")
-
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: xli \"your post here\"")
 		os.Exit(1)
 	}
 
 	input := os.Args[1]
-	if input == "login" {
-		// LoginHandler()
-	}
+	// if input == "login" {
+	// 	// LoginHandler()
+	// }
 
 	// RefreshToken if needed
 	// SendPost
